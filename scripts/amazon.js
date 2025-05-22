ccff20ae8203cae2f4feed5da06a2bbe7f2db50c
Name1 <sreejabodasu@gmail.com>
2025-05-22 16:26:05 +0530

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -74,7 +74,14 @@ button.addEventListener('click',()=>{
       quantity:1
   });
   }
+
+
+  let cartQuantity=0;
+  cart.forEach((item)=>{
+    cartQuantity+=item.quantity;
+  });
+
+  document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
   
-  console.log(cart);
   });
 });
