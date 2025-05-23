894c045cfd1762c354951f5797552482e43d6224
Name1 <sreejabodasu@gmail.com>
2025-05-23 19:22:50 +0530

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -4,11 +4,8 @@
   import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
   import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
   import {deliveryOptions,getDeliveryOption} from '../../data/deliveryOptions.js';
+  import { renderPaymentSummary } from './paymentSummary.js';
 
-  hello();
-  const today=dayjs();
-  const deliveryDate=today.add(7, 'days');
-  console.log(deliveryDate.format('dddd,MMMM D'));
 
   export function renderOrderSummary(){
     let cartSummaryHTML='';
@@ -127,6 +124,7 @@
       removeFromCart(productId);
       const container=document.querySelector(`.js-cart-item-container-${productId}`);
       container.remove();
+      renderPaymentSummary();
     });
     });
 
@@ -136,6 +134,7 @@
           const {productId,deliveryOptionId}=element.dataset;
             updateDeliveryOption(productId,deliveryOptionId);
             renderOrderSummary();
+            renderPaymentSummary();
         });
     });
   }
