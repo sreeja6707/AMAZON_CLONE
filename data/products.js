1cf7ce5ade0e57a6b226379b0b27117bfb3e0a92
Name1 <sreejabodasu@gmail.com>
2025-06-02 20:01:31 +0530

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -99,9 +99,13 @@ export function loadProductsFetch() {
   });
     console.log('load products');
     
+  }).catch((error)=>{
+    console.log('Unexpected error. Please try again later.');
   });
   return promise;
 }
+
+/*
 loadProductsFetch().then(() => {
   console.log('next step');
 });
@@ -145,13 +149,17 @@ export function loadProducts(fun){
     fun();
   });
 
-
   xhr.addEventListener('error', (error) => {
     console.log('Unexpected error. Please try again later.');
   });
+
+  /*xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try again later.');
+  });*/
   xhr.open('GET','https://supersimplebackend.dev/products');
   xhr.send();
 }
+
 
 
 /*
